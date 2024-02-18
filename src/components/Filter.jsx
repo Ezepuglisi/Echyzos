a26ba7d0cd5502215170f9d882a0096136210092
Ezequiel <ezepuglisi@gmail.com>
2024-02-18 01:23:21 -0300

    fixed key

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Filter = ({data}) => {
   return (
     <div className='hidden lg:flex w-full justify-center gap-6 bg-white shadow-md border py-1'>
-        {data.map((filtro) => {
-            return <p className='text-xs hover:bg-green-200 px-2 py-1 rounded-md cursor-pointer'>{filtro.name}</p>
+        {data.map((filtro, index) => {
+            return <p key={index} className='text-xs hover:bg-green-200 px-2 py-1 rounded-md cursor-pointer'>{filtro.name}</p>
         })}
     </div>
   )
