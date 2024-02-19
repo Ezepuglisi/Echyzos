ecb2a5f8d32d2d608472b9494d3b4fb9d603cfcd
Ezequiel <ezepuglisi@gmail.com>
2024-02-19 19:46:42 -0300

    fix deployment

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,29 +1,40 @@
+'use client'
 import ProductContainer from "@/components/ProductContainer"
 // import prisma from "@/libs/db"
 
 
-export default async function Home() {
+export default function Home() {
 
-  const products = await prisma.product.findMany({
-    include: {
-      productModels: true,
-      colors: {
-        include: {
-          color: true,
-          images: true
-        }
-      },
-      tags: true,
-      generalImages: true
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const getProductsFromDB = async () => {
+      const result = await getProducts()
+
+      if (result.error) {
+        setLoading(false)
+        return
+      }
+
+      setData(result)
+      setLoading(false)
+
     }
-  })
 
-  console.log(products)
+    getProductsFromDB()
+  }, [])
 
-  
+
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-10">
-        <ProductContainer products={products} />
+      {
+        data.length > 0 ?
+          <ProductContainer products={data} /> 
+          :
+          <p>loading</p>
+      }
     </div>
   )
 }
